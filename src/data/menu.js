0918c44954ef4bfba3ea5621668be75cbbad23f7
Kaan Koç <kaankoc@Kaan-MacBook-Air.local>
2025-05-30 00:13:13 +0300

    bb7

--- src/data/menu.js
+++ src/data/menu.js
@@ -2,18 +2,21 @@
 
 const menu = [
     {
-      category: "Lahmacun",
+      category: "Yemekler",
       items: [
         { name: "Kıymalı Lahmacun", price: 40 },
-        { name: "Kaşarlı Lahmacun", price: 45 }
-      ]
-    },
-    {
-      category: "Pide",
-      items: [
+        { name: "Kaşarlı Lahmacun", price: 45 },
         { name: "Kıymalı Pide", price: 60 },
         { name: "Kuşbaşılı Pide", price: 70 },
         { name: "Kaşarlı Pide", price: 65 }
+      ]
+    },
+    {
+      category: "Nargileler",
+      items: [
+        { name: "freshnar", price: 60 },
+        { name: "blackrock", price: 70 },
+        { name: "Elfakir", price: 65 }
       ]
     },
     {
